750dc97514cb27071a8a55e8369dd23b0cf2124c
Ani <aavetisian@noorlogic.com>
2023-04-26 16:58:42 +0400

    feat: add keyboard block

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,7 @@ const headingText = document.createTextNode("Type by clicking mouse or keyboard"
 body.appendChild(heading)
 heading.append(headingText);
 
-//textarea
+//textarea block
 const para = document.createElement("p");
 const textArea = document.createElement("textarea");
 const teaserText = document.createTextNode("text");
@@ -18,5 +18,17 @@ body.appendChild(textAreaBlock);
 textAreaBlock.appendChild(para);
 textAreaBlock.appendChild(textArea);
 para.append(teaserText);
-textAreaBlock.classList.add("text_area_block")
+textAreaBlock.classList.add("text_area_block");
 
+//keyboard
+const keyboardBlock = document.createElement("div");
+keyboardBlock.classList.add("keyboard-block");
+body.appendChild(keyboardBlock);
+
+const keyLayout = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+    "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+    "space"
+];
